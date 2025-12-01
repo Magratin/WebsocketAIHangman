bcaaf9324d70d6e20ae0aed05f5d03c8a4995bc0
Magratin <liupetery@gmail.com>
2025-12-01 10:38:31 +0100

    axios

---
+++ HangingYarakMan/src/kuhlschum.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const prompt = "Generiere%20mir%20ein%20sechs%20Zeichen%20langes%20deutsches%20zuffälliges%20Wort%20nur%20das%20Wort%20ausgeben."
+const url = `text.pollinations.ai/${encodeURIComponent(prompt)}?model=openai&temperature=1.0`;
+let result = "";
+
+function getHangmanKeyword() {
+    axios.post(url)
+        .then(response => result = response)
+        .then(response => console.log('Word:', response))
+        .catch(error => console.error('Error:', error));
+    return result;
+}
