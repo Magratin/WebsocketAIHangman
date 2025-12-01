67ba1d9c3555227f942d349c88e2041b5a3cd3bc
Magratin <liupetery@gmail.com>
2025-12-01 10:45:20 +0100

    Update kuhlschum.js

--- HangingYarakMan/src/kuhlschum.js
+++ HangingYarakMan/src/kuhlschum.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const prompt = "Generiere%20mir%20ein%20sechs%20Zeichen%20langes%20deutsches%20zuffälliges%20Wort%20nur%20das%20Wort%20ausgeben."
-const url = `text.pollinations.ai/${encodeURIComponent(prompt)}?model=openai&temperature=1.0`;
+const url = "https://text.pollinations.ai/${encodeURIComponent(prompt)}?model=openai&temperature=1.0";
 let result = "";
 
-function getHangmanKeyword() {
+export default function getHangmanKeyword() {
     axios.post(url)
         .then(response => result = response)
         .then(response => console.log('Word:', response))
